746bd6b4eb1d2b4249b21b00e62c1e9dca6d6e09
JessyPS <89113356+JessyPS@users.noreply.github.com>
2021-08-30 12:07:25 -0300

    Fazer com que contatos seja dinâmico

--- src/app/components/lista-de-contatos/lista-de-contatos.component.ts
+++ src/app/components/lista-de-contatos/lista-de-contatos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contato } from 'src/app/models/Contato';
 
 @Component({
   selector: 'app-lista-de-contatos',
@@ -6,6 +7,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-de-contatos.component.css']
 })
 export class ListaDeContatosComponent implements OnInit {
+
+  contatos: Contato[]= [
+    {
+      nome:"Jessica Pereira",
+      email: "florjess@teste.com",
+      telefones: ["55555-5555"]
+    },
+    {
+      nome:"Jessica Pereira",
+      email: "florjess31@teste.com",
+      telefones: ["32222-5555"]
+    },
+    {
+      nome:"Jessica Pereira",
+      email: "florjess31@teste.com",
+      telefones: ["44444-5555"]
+    },
+    {
+      nome:"Jessica Pereira",
+      email: "florjess31@teste.com",
+      telefones: ["11111-5555"]
+    },
+    {
+      nome:"Jessica Pereira",
+      email: "florjess31@teste.com",
+      telefones: ["99999-5555"]
+    },
+    {
+      nome:"Jessica Pereira",
+      email: "florjess31@teste.com",
+      telefones: ["22222-5555"]
+    },
+    {
+      nome:"Jessica Pereira",
+      email: "florjess31@teste.com",
+      telefones: ["48545-5555"]
+    },
+    {
+      nome:"Jessica Pereira",
+      email: "florjess31@teste.com",
+      telefones: ["44848-5555"]
+    },
+    {
+      nome:"Jessica Pereira",
+      email: "florjess31@teste.com",
+      telefones: ["31313-5555"]
+    },
+    {
+      nome:"Jessica Pereira",
+      email: "florjess31@teste.com",
+      telefones: ["57842-5555"]
+    },
+    {
+      nome:"Jessica Pereira",
+      email: "florjess31@teste.com",
+      telefones: ["335541-5555"]
+    },
+    {
+      nome:"Jessica Pereira",
+      email: "florjess31@teste.com",
+      telefones: ["58755-5555"]
+    }
+  ]
 
   constructor() { }
 
