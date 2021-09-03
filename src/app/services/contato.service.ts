57c9d1d3c6a441f032582d2749e850e46dbb9cfe
JessyPS <89113356+JessyPS@users.noreply.github.com>
2021-09-03 17:21:41 -0300

    Adicionando e exibindo novos contatos

--- src/app/services/contato.service.ts
+++ src/app/services/contato.service.ts
@@ -1,53 +1,53 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Contato } from '../models/Contato';
 
-const BASE_DE_CONTATOS:Contato[] = [
-  {
-    nome: "Mariana Ribeiro 1",
-    email: "mariana@teste.com",
-    telefones: ['1111 - 1111','1111 - 1111','1111 - 1111']
-  },
-  {
-    nome: "Mariana Ribeiro 2",
-    email: "mariana@teste.com",
-    telefones: ['2222 - 2222']
-  },
-  {
-    nome: "Mariana Ribeiro 3",
-    email: "mariana@teste.com",
-    telefones: ['3333 - 3333']
-  },
-  {
-    nome: "Mariana Ribeiro 4",
-    email: "mariana@teste.com",
-    telefones: ['4444 - 4444']
-  },
-  {
-    nome: "Mariana Ribeiro 5",
-    email: "mariana@teste.com",
-    telefones: ['5555 - 5555']
-  },
-  {
-    nome: "Mariana Ribeiro 6",
-    email: "mariana@teste.com",
-    telefones: ['6666 - 6666']
-  },
-  {
-    nome: "Mariana Ribeiro 9",
-    email: "mariana@teste.com",
-    telefones: ['9999 - 9999']
-  },
-  {
-    nome: "Mariana Ribeiro 7",
-    email: "mariana@teste.com",
-    telefones: ['7777 - 7777']
-  },
-  {
-    nome: "Mariana Ribeiro 8",
-    email: "mariana@teste.com",
-    telefones: ['8888 - 8888']
-  },
-]
+// const BASE_DE_CONTATOS:Contato[] = [
+//   {
+//     nome: "Mariana Ribeiro 1",
+//     email: "mariana@teste.com",
+//     telefones: ['1111 - 1111','1111 - 1111','1111 - 1111']
+//   },
+//   {
+//     nome: "Mariana Ribeiro 2",
+//     email: "mariana@teste.com",
+//     telefones: ['2222 - 2222']
+//   },
+//   {
+//     nome: "Mariana Ribeiro 3",
+//     email: "mariana@teste.com",
+//     telefones: ['3333 - 3333']
+//   },
+//   {
+//     nome: "Mariana Ribeiro 4",
+//     email: "mariana@teste.com",
+//     telefones: ['4444 - 4444']
+//   },
+//   {
+//     nome: "Mariana Ribeiro 5",
+//     email: "mariana@teste.com",
+//     telefones: ['5555 - 5555']
+//   },
+//   {
+//     nome: "Mariana Ribeiro 6",
+//     email: "mariana@teste.com",
+//     telefones: ['6666 - 6666']
+//   },
+//   {
+//     nome: "Mariana Ribeiro 9",
+//     email: "mariana@teste.com",
+//     telefones: ['9999 - 9999']
+//   },
+//   {
+//     nome: "Mariana Ribeiro 7",
+//     email: "mariana@teste.com",
+//     telefones: ['7777 - 7777']
+//   },
+//   {
+//     nome: "Mariana Ribeiro 8",
+//     email: "mariana@teste.com",
+//     telefones: ['8888 - 8888']
+//   },
+// ]
 
 @Injectable({
   providedIn: 'root'
@@ -55,6 +55,8 @@ const BASE_DE_CONTATOS:Contato[] = [
 export class ContatoService {
 
   private readonly chave:string = "CONTATOS";
+
+  static onContatosAlterados: EventEmitter<Contato[]> = new EventEmitter();
 
   constructor() { }
   getContatos():Contato[] {
@@ -73,9 +75,7 @@ export class ContatoService {
     // Se não houver dados => (Guardo uma array vazia no localStorage; Retorna uma array vazia)
       window.localStorage.setItem(this.chave, "[]");
       return [];
-
     }
-
   }
 
   addContato(c:Contato): void {
@@ -88,6 +88,9 @@ export class ContatoService {
 
     // Salvar o array de volta no localStorage
     window.localStorage.setItem(this.chave, JSON.stringify(contatos));
+
+    // Emitindo evento "contados alterados"
+    ContatoService.onContatosAlterados.emit(contatos);
   }
 
 }
